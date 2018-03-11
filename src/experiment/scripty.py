0a71a737064cee8c15f928bd8963499674889bf9
mattall <mhhall92@gmail.com>
2018-03-10 18:00:10 -0800

    edited scripty, fixed type-o

--- src/experiment/scripty.py
+++ src/experiment/scripty.py
@@ -8,14 +8,20 @@ https://www.electricmonk.nl/log/2014/07/26/scripting-a-cisco-switch-with-python-
 
 switch_ip = "192.168.57.200"
 switch_pw = "cisco"
+
 port_in = "GigabitEthernet 0/1"
 port_out = "GigabitEthernet 0/28"
-vlan_id = 100
+ports = [port_in, port_out]
+
+#vlan_id = 100
 verbose = True
 subnet = "192.168.57."
 suffix = str(2)
-megabytes = 25 # megabytes
+megabytes = 1 # megabytes
 bits = megabytes * 8 * 1000000
+# what percent of 125 is the request
+# ports are GigabitEthernet, so max bandwithd is 125.
+request = int(float(megabytes) // 125 * 100)
 
 try:
     try:
@@ -35,49 +41,66 @@ try:
     child.expect('Password:')
     child.sendline(switch_pw)
     child.expect('#')
-    child.sendline('show running-config interface GigabitEthernet 0/1')
-    child.sendline('conf t')
+    child.sendline('configure terminal')
     child.expect('\(config\)#')
 
     # Create a vlan
-    try:
-        child.sendline('int vlan %d', vlan_id)
-        o = child.expect(['\(config-if\)#', '% Invalid'])
-        if o != 0:
-            raise Exception("invalid vlan number '%s', must be 1 - 4094." % (port))
-        child.sendline("ip address " + subnet + suffix)
-        o = child.expect(['\(config-if\)#', '% Invalid'])
-        if o != 0:
-            raise Exception("Error addressing vlan")
-        child.sendline("no shut")
-        child.expect('\(config-if\)#')
-        try:
-            child.sendline("rate-lime input {} {} {} conform-action transmit exceed-action drop".format(bits, bits/2000, bits/2000))
-            child.expect('\(config-if\)#')
-            child.sendline("rate-lime output {} {} {} conform-action transmit exceed-action drop".format(bits, bits/2000, bits/2000))
-            child.expect('\(config-if\)#')
-            child.sendline("end")
-            child.expect('\(config\)#')
-        except:
-            raise Exception("Error assigning bitrate on vlan")
-    except:
-        raise Exception("Error creating vlan.")
+    # try:
+    #     child.sendline('int vlan %d', vlan_id)
+    #     o = child.expect(['\(config-if\)#', '% Invalid'])
+    #     if o != 0:
+    #         raise Exception("invalid vlan number '%s', must be 1 - 4094." % (port))
+    #     child.sendline("ip address " + subnet + suffix)
+    #     o = child.expect(['\(config-if\)#', '% Invalid'])
+    #     if o != 0:
+    #         raise Exception("Error addressing vlan")
+    #     child.sendline("no shut")
+    #     child.expect('\(config-if\)#')
+    #     child.sendline("end")
+    #     child.expect('\(config\)#')
+    #     # try:
+    #     #     child.sendline("rate-lime input {} {} {} conform-action transmit exceed-action drop".format(bits, bits/2000, bits/2000))
+    #     #     child.expect('\(config-if\)#')
+    #     #     child.sendline("rate-lime output {} {} {} conform-action transmit exceed-action drop".format(bits, bits/2000, bits/2000))
+    #     #     child.expect('\(config-if\)#')
+    #     #     child.sendline("end")
+    #     #     child.expect('\(config\)#')
+    #     # except:
+    #     #     raise Exception("Error assigning bitrate on vlan")
+    # except:
+    #     raise Exception("Error creating vlan.")
 
     # Allowing Vlan on interface
-    child.sendline('interface %s' % (port_in))
-    o = child.expect(['\(config-if\)#', '% Invalid'])
-    if o != 0:
-        raise Exception("Unknown switch port '%s'" % (port))
-    child.sendline('switchport access vlan %s' % (vlan))
-    child.expect('\(config-if\)#')
-    child.sendline('no shutdown')
-    child.expect('#')
-    child.sendline('end')
-    child.expect('#')
-    child.sendline('wr mem')
-    child.expect('[OK]')
-    child.expect('#')
-    child.sendline('quit')
-
+    for p in ports:
+        child.sendline("show mls qos interface {} queueing | include bandwidth".format(p))
+        rate_description = child.readline();
+        rate = re.finall("\d+", rate_description)[0]
+        child.expect('\(config\)#')
+        child.sendline('interface %s' % (p))
+        o = child.expect(['\(config-if\)#', '% Invalid'])
+        if o != 0:
+            raise Exception("Unknown switch port '%s'" % (port))
+        # child.sendline('switchport access vlan %s' % (vlan_id))
+        child.expect('\(config-if\)#')
+        child.sendline('no shutdown')
+        child.expect('\(config-if\)#')
+        try:
+            new_rate = rate + request
+            assert new_rate <= 100
+            if new_rate > 90:
+                child.sendline('no srr-queue bandwidth limit')
+                child.expect('\(config-if\)#')
+            elif new_rate >= 10:
+                child.sendline('srr-queue bandwidth limit {}'.format(new_rate))
+                child.expect('\(config-if\)#')
+            else:
+                raise Exception("Error encoutered allocating bandwidth on port {}".format(p))
+        except AssertionError:
+            raise Exception("requested bandwidth not available")
+            child.sendline("show mls qos interface {} queueing | include bandwidth".format(p))
+        child.sendline('wr mem')
+        child.expect('[OK]')
+        child.expect('#')
+        child.sendline('quit')
 except (pexpect.EOF, pexpect.TIMEOUT), e:
     raise error("Error while trying to move the vlan on the switch.")
