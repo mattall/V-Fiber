348e0a79b4759ed4ddf71379110c85a83a4a6828
mattall <mhhall92@gmail.com>
2018-03-15 15:15:50 -0700

    added extinguish

--- src/experiment/torchbearer.py
+++ src/experiment/torchbearer.py
@@ -24,17 +24,17 @@ def light_path(ips = ["192.168.57.200", "192.168.57.201"], port = "GigabitEthern
         try:
             child.sendline(switch_pw)
             child.expect('>')
-            child.sendline('terminal length 0')
+            child.sendline('term length 0')
             child.expect('>')
             child.sendline('enable')
             child.expect('Password:')
             child.sendline(switch_pw)
             child.expect('#')
-            child.sendline('configure terminal')
+            child.sendline('conf t')
             child.expect('\(config\)#')
-            child.sendline('interface %s' % (swith_port))
+            child.sendline('int %s' % (swith_port))
             child.expect('\(config-if\)#')
-            child.sendline('no shutdown')
+            child.sendline('no shut')
             child.expect('\(config-if\)#')
             child.sendline('end')
             child.expect('#')
@@ -45,3 +45,44 @@ def light_path(ips = ["192.168.57.200", "192.168.57.201"], port = "GigabitEthern
         except (pexpect.EOF, pexpect.TIMEOUT), e:
             child.close()
             raise Exception("Error while trying to move the vlan on the switch.")
+
+def extinguish_path(ips = ["192.168.57.200", "192.168.57.201"], port = "GigabitEthernet 0/28"):
+        switches = ips
+        switch_pw = "cisco"
+        swith_port = port
+        verbose = True
+
+        for addr in switches:
+            try:
+                child = pexpect.spawn('telnet %s' % (addr))
+                if verbose:
+                    child.logfile = sys.stdout
+                child.timeout = 4
+                child.expect('Password:')
+            except pexpect.TIMEOUT:
+                raise Exception("Couldn't log on to the switch")
+
+            try:
+                child.sendline(switch_pw)
+                child.expect('>')
+                child.sendline('term length 0')
+                child.expect('>')
+                child.sendline('enable')
+                child.expect('Password:')
+                child.sendline(switch_pw)
+                child.expect('#')
+                child.sendline('conf t')
+                child.expect('\(config\)#')
+                child.sendline('interface %s' % (swith_port))
+                child.expect('\(config-if\)#')
+                child.sendline('shut')
+                child.expect('\(config-if\)#')
+                child.sendline('end')
+                child.expect('#')
+                child.sendline('wr mem')
+                child.expect('[OK]')
+                child.expect('#')
+                child.sendline('quit')
+            except (pexpect.EOF, pexpect.TIMEOUT), e:
+                child.close()
+                raise Exception("Error while trying to move the vlan on the switch.")
