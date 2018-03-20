9ddbc0053b3dfb480124fac11e62db9357177e3e
mattall <mhhall92@gmail.com>
2018-03-20 11:22:45 -0700

    added qos bandwidth to light_path

--- src/experiment/torchbearer.py
+++ src/experiment/torchbearer.py
@@ -7,7 +7,13 @@ from time import sleep
 '''
 Toarchbearer lights an end-to-end path of dark fiber
 '''
-def light_path(ips = ["192.168.57.200", "192.168.57.201"], port = "GigabitEthernet 0/28"):
+def light_path(ips = ["192.168.57.200", "192.168.57.201"], port = "GigabitEthernet 0/28", request_size = 1):
+    megabytes = request_size
+    bits = megabytes * 8 * 1000000
+    # what percent of 125 is the request
+    # ports are GigabitEthernet, so max bandwithd is 125 megabytes.
+    request = int(float(megabytes) // 125 * 100) # request expressed as percent of max bandwidth.
+
     switches = ips
     switch_pw = "cisco"
     swith_port = port
@@ -23,6 +29,7 @@ def light_path(ips = ["192.168.57.200", "192.168.57.201"], port = "GigabitEthern
         except pexpect.TIMEOUT:
             raise Exception("Couldn't log on to the switch")
 
+
         try:
             child.sendline(switch_pw)
             child.expect('>')
@@ -32,6 +39,37 @@ def light_path(ips = ["192.168.57.200", "192.168.57.201"], port = "GigabitEthern
             child.expect('Password:')
             child.sendline(switch_pw)
             child.expect('#')
+            child.sendline("show mls qos interface {} queueing | include bandwidth".format(swith_port))
+            rate_description = child.read();
+            rate = int(re.findall("\d+", rate_description)[0])
+            child.expect('#')
+            child.sendline('configure terminal')
+            child.expect('\(config\)#')
+            child.sendline('interface %s' % (p))
+            o = child.expect(['\(config-if\)#', '% Invalid'])
+            if o != 0:
+                raise Exception("Unknown switch port '%s'" % (port))
+            child.sendline('no shutdown')
+            child.expect('\(config-if\)#')
+            try:
+                new_rate = rate + request
+                assert new_rate <= 100
+                if new_rate > 90:
+                    child.sendline('no srr-queue bandwidth limit')
+                    child.expect('\(config-if\)#')
+                elif new_rate >= 10:
+                    child.sendline('srr-queue bandwidth limit {}'.format(new_rate))
+                    child.expect('\(config-if\)#')
+                else:
+                    raise Exception("Error encoutered allocating bandwidth on port {}".format(p))
+            except AssertionError:
+                raise Exception("requested bandwidth not available")
+                child.sendline("show mls qos interface {} queueing | include bandwidth".format(p))
+            child.sendline('wr mem')
+            child.expect('[OK]')
+            child.expect('#')
+            child.sendline('end')
+
             child.sendline('conf t')
             child.expect('\(config\)#')
             child.sendline('int %s' % (swith_port))
