bce0819a1b38845dc1e3702f3bc8dc186c8ee134
mattall <mhhall92@gmail.com>
2018-03-20 12:04:14 -0700

    fixed indentation

--- src/experiment/torchbearer.py
+++ src/experiment/torchbearer.py
@@ -20,75 +20,75 @@ def light_path(ips = ["192.168.57.200", "192.168.57.201"], port = "GigabitEthern
     verbose = False
 
     for addr in switches:
-    try:
         try:
-            child = pexpect.spawn('telnet %s' % (addr))
-            if verbose:
-                child.logfile = sys.stdout
-            child.timeout = 4
-            child.expect('Password:')
-        except pexpect.TIMEOUT:
-            raise Exception("Couldn't log on to the switch")
+            try:
+                child = pexpect.spawn('telnet %s' % (addr))
+                if verbose:
+                    child.logfile = sys.stdout
+                child.timeout = 4
+                child.expect('Password:')
+            except pexpect.TIMEOUT:
+                raise Exception("Couldn't log on to the switch")
 
 
-        try:
-            child.sendline(switch_pw)
-            child.expect('>')
-            child.sendline('term length 0')
-            child.expect('>')
-            child.sendline('enable')
-            child.expect('Password:')
-            child.sendline(switch_pw)
-            child.expect('#')
-            '''
-            Is port on or off? If port is off, turn on and set bandwidth limit to requested limit.
-            If it is on, find the rate limit and increase it appropriatly.
-            '''
-            child.sendline("show running-config int {}".format(switch_port))
-            status = child.read()
-            if "shutdown" in status:
-                rate = 0
-                child.sendline('configure terminal')
-                child.expect('\(config\)#')
-                child.sendline('interface %s' % (p))
-                o = child.expect(['\(config-if\)#', '% Invalid'])
-                if o != 0:
-                    raise Exception("Unknown switch port '%s'" % (port))
-                child.sendline('no shutdown')
-                child.expect('\(config-if\)#')
-            else:
-                child.sendline("show mls qos interface {} queueing | include bandwidth".format(swith_port))
-                rate_description = child.read();
-                rate = int(re.findall("\d+", rate_description)[0])
+            try:
+                child.sendline(switch_pw)
+                child.expect('>')
+                child.sendline('term length 0')
+                child.expect('>')
+                child.sendline('enable')
+                child.expect('Password:')
+                child.sendline(switch_pw)
                 child.expect('#')
-                child.sendline('configure terminal')
-                child.expect('\(config\)#')
-                child.sendline('interface %s' % (p))
-                o = child.expect(['\(config-if\)#', '% Invalid'])
-                if o != 0:
-                    raise Exception("Unknown switch port '%s'" % (port))
-            new_rate = rate + request
-            assert new_rate <= 100
-            if new_rate > 90:
-                child.sendline('no srr-queue bandwidth limit')
-                child.expect('\(config-if\)#')
-            elif new_rate >= 10:
-                child.sendline('srr-queue bandwidth limit {}'.format(new_rate))
-                child.expect('\(config-if\)#')
-            else:
-                raise Exception("Error encoutered allocating bandwidth on port {}".format(p))
-        except AssertionError:
-            raise Exception("Error configuring switch port")
-            child.sendline("show mls qos interface {} queueing | include bandwidth".format(p))
-        child.sendline('end')
-        child.expect('#')
-        child.sendline('wr mem')
-        child.expect('[OK]')
-        child.expect('#')
-        child.sendline('quit')
-    except (pexpect.EOF, pexpect.TIMEOUT), e:
-        child.close()
-        raise Exception("Error while trying to move the vlan on the switch.")
+                '''
+                Is port on or off? If port is off, turn on and set bandwidth limit to requested limit.
+                If it is on, find the rate limit and increase it appropriatly.
+                '''
+                child.sendline("show running-config int {}".format(switch_port))
+                status = child.read()
+                if "shutdown" in status:
+                    rate = 0
+                    child.sendline('configure terminal')
+                    child.expect('\(config\)#')
+                    child.sendline('interface %s' % (p))
+                    o = child.expect(['\(config-if\)#', '% Invalid'])
+                    if o != 0:
+                        raise Exception("Unknown switch port '%s'" % (port))
+                    child.sendline('no shutdown')
+                    child.expect('\(config-if\)#')
+                else:
+                    child.sendline("show mls qos interface {} queueing | include bandwidth".format(swith_port))
+                    rate_description = child.read();
+                    rate = int(re.findall("\d+", rate_description)[0])
+                    child.expect('#')
+                    child.sendline('configure terminal')
+                    child.expect('\(config\)#')
+                    child.sendline('interface %s' % (p))
+                    o = child.expect(['\(config-if\)#', '% Invalid'])
+                    if o != 0:
+                        raise Exception("Unknown switch port '%s'" % (port))
+                new_rate = rate + request
+                assert new_rate <= 100
+                if new_rate > 90:
+                    child.sendline('no srr-queue bandwidth limit')
+                    child.expect('\(config-if\)#')
+                elif new_rate >= 10:
+                    child.sendline('srr-queue bandwidth limit {}'.format(new_rate))
+                    child.expect('\(config-if\)#')
+                else:
+                    raise Exception("Error encoutered allocating bandwidth on port {}".format(p))
+            except AssertionError:
+                raise Exception("Error configuring switch port")
+                child.sendline("show mls qos interface {} queueing | include bandwidth".format(p))
+            child.sendline('end')
+            child.expect('#')
+            child.sendline('wr mem')
+            child.expect('[OK]')
+            child.expect('#')
+            child.sendline('quit')
+        except (pexpect.EOF, pexpect.TIMEOUT), e:
+            child.close()
+            raise Exception("Error while trying to move the vlan on the switch.")
 
 def fast_extinguish_path(ips = ["192.168.57.200", "192.168.57.201"], port = "GigabitEthernet 0/28"):
         ''' Doesn't write config to memory '''
