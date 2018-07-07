6da3eedd8ff9ed60c10ba71afde73c5b2fd6ab3d
mattall <mhhall92@gmail.com>
2018-07-07 16:43:14 -0700

    extensions

--- src/experiment/ping-test.py
+++ src/experiment/ping-test.py
@@ -5,16 +5,28 @@ import argparse
 from a_timer import Timer
 from torchbearer import light_path, extinguish_path
 from multiprocessing import Process
+from subprocess import call
+from numpy import mean, std
+from server_details import SERVERS
+from time import sleep
+from exp_utilities import get_ssh_connection
 
 file_num = 0
 
 def ping(host, time):
-    global file_nun
+    global file_num
     count = time * 10 # send ten requests per second
     outfile = "ping_{}".format(file_num)
     with open(outfile, 'w') as fout:
-        subprocess.call(["ping", "host", "-i 0.1" "-c {}".format(count)], stdout = fout)
+        call(["ping", "host", "-i 0.1 " " -c {}".format(count)], stdout = fout)
     file_num += 1
+
+def light_path((sshConnObject, addr1, addr2, int1, int2, password)):
+    return sshConnObject.exec_command("python torchbearer.py -m l -a1 {} -a2 {} -i1 {} -i2 {} -s n -v y -p {}".format(addr1, addr2, int1, int2, password))
+
+def extinguish_path((sshConnObject, mode, addr1, addr2, int1, int2, password)):
+    return sshConnObject.exec_command("python torchbearer.py -m e -a1 {} -a2 {} -i1 {} -i2 {} -s n -v n -p {}".format(addr1, addr2, int1, int2, password))
+
 
 def main(args):
     endpoint = args.endpoint
@@ -24,32 +36,18 @@ def main(args):
     print("running experiment \n endpoint = {} \n runs = {} \n debug = {} \
              delta = {}".format(endpoint, runs, debug, delta))
 
-    test_interfaces = [("192.168.57.200", "GigabitEthernet 0/25"),
-                                    ("192.168.57.201","GigabitEthernet 0/25")]
+    ssh = get_ssh_connection('matt', '192.168.57.102', 'onrgserver1')
+    test_tuple = (ssh, "192.168.57.200", "192.168.57.201", "GigabitEthernet 0/25", "GigabitEthernet 0/25", "cisco")
 
-    # extinguish all paths to endpoint
-    extinguish_path(ip_port_pairs = [("192.168.57.200", "GigabitEthernet 0/25"), ("192.168.57.201","GigabitEthernet 0/25"),\
-                    ("192.168.57.200", "GigabitEthernet 0/26"), ("192.168.57.201","GigabitEthernet 0/26"),\
-                    ("192.168.57.200", "GigabitEthernet 0/27"), ("192.168.57.201","GigabitEthernet 0/27"),\
-                    ("192.168.57.200", "GigabitEthernet 0/28"), ("192.168.57.201","GigabitEthernet 0/28")],\
-                    save = False, password = "cisco", disply_output = False)
-
-    sleep(2)
-
-    # light one end-to-end path
-    light_path(test_interfaces)
-
+    stdin, stdout, stderr = ssh.exec_command("cd /home/matt/vFiber/V-Fiber/src/experiment; source ../../bin/activate")
+    stdin, stdout, stderr = light_path(test_tuple)
+    sleep(40)
     # begin experiment
     for _ in range(runs):
         p = Process(target=ping, args=(endpoint, delta))
-        e = Process(target=extinguish, args=(path))
-        l = Process(target=light_path, args=(test_interfaces))
         p.start()
-        e.start()
-        e.joint()
-        l.start()
-        sleep(delta)
-        l.join()
+        extinguish_path(test_tuple)
+        light_path(test_tuple)
         p.join()
 
     global file_num
@@ -66,7 +64,6 @@ def main(args):
                 min = f_data[i]
             else: pass
 
-    from numpy import mean, std
     average = mean(times)
     standard_deviation = std(times)
     print("mean: {}".format(mean))
