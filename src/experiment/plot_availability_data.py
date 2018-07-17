5e73eada7301dccc3a8b2a3aeadb6c371ede65e2
mattall <mhhall92@gmail.com>
2018-07-17 13:52:08 -0700

    render graphs with larger font and increased space between axis and label

--- src/experiment/plot_availability_data.py
+++ src/experiment/plot_availability_data.py
@@ -23,7 +23,7 @@ def create_plot(filename, bucketSize=5, starts=[], stops=[]):
         # minus the second entry of the (x-1)'th tuple
         completed.append(activity_log[x][1]-activity_log[x-bucketSize][1])
 
-    mpl.rcParams.update({'font.size': 45})
+    mpl.rcParams.update({'font.size': 60})
     mpl.rcParams['axes.linewidth'] = 4
     plt.rcParams["font.family"] = "Times New Roman"
 
@@ -44,21 +44,25 @@ def create_plot(filename, bucketSize=5, starts=[], stops=[]):
     for s in stops:
         plt.axvline(x=s, color = 'red', linewidth=lw, linestyle='--')
 
-    plt.gcf().subplots_adjust(bottom = 0.18, left = 0.18)
-    plt.xlabel('Time (seconds)', fontsize = label_size)
-    plt.ylabel('Completed Requests \n per {} Seconds'.format(bucketSize), fontsize = label_size)
+    plt.gcf().subplots_adjust(bottom = 0.21, left = 0.20)
+    plt.xlabel('Time (seconds)', fontsize = label_size, labelpad=30)
+    plt.ylabel('Completed Requests \n per {} Seconds'.format(bucketSize), fontsize = label_size, labelpad=25)
 
     plt.plot(time_t, completed)
     plt.show()
 
 def main():
-    fname = "availability_experiment_length-600:delta-1:volume-100:failure_testing-False:poisson-True.pkl"
-    # fname = "availability_experiment_length-600:delta-1:volume-100:failure_testing-True:poisson-True.pkl"
-    # starts = [120,240,360,480]
-    # stops = [60,180,300,420,540]
-    # create_plot(fname, 5, starts, stops)
-    create_plot(fname, 5)
+    ## NO FAIL
+    # fname = "availability_experiment_length-600:delta-1:volume-100:failure_testing-False:poisson-True.pkl"
+    # create_plot(fname, 5)
 
+    # Periodic Failure/Recovery
+    fname = "availability_experiment_length-600:delta-1:volume-100:failure_testing-True:poisson-True.pkl"
+    starts = [120,240,360,480]
+    stops = [60,180,300,420,540]
+    create_plot(fname, 5, starts, stops)
+
+    # # DEADSTOP TEST
     # fname = "availability_experiment_length-600:delta-1:volume-100:failure_testing-d:poisson-True.pkl"
     # starts = [150, 200, 450]
     # stops = [60, 120, 420]
